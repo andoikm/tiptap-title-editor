e8ebdb746d2858fdd896e7d3c15a1ae8d6a216c4
Andranik Minasyan <andranik.minasyan@lynon.com>
2025-08-07 00:57:39 +0400

    Add eslint and prettier integration for code linting and formatting

--- src/components/TiptapEditor.jsx
+++ src/components/TiptapEditor.jsx
@@ -1,21 +1,22 @@
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import { TitleMark } from '../extensions/TitleMark'
-import { useTooltipPlugin } from './TooltipPlugin'
-import TitleButton from './TitleButton'
-import {Fragment, useState} from "react";
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { Fragment, useState } from 'react';
+
+import { TitleMark } from '../extensions/TitleMark';
+
+import TitleButton from './TitleButton';
+import { useTooltipPlugin } from './TooltipPlugin';
 
 const TiptapEditor = () => {
   const [state, setState] = useState(null);
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      TitleMark,
-    ],
-    content: '<p>Hello World! 👋</p><p>Select some text and try the Title feature!</p>',
+    extensions: [StarterKit, TitleMark],
+    content:
+      '<p>Hello World! 👋</p><p>Select some text and try the Title feature!</p>',
     editorProps: {
       attributes: {
-        class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+        class:
+          'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
       },
     },
     onUpdate: ({ editor }) => {
@@ -23,30 +24,24 @@ const TiptapEditor = () => {
       setState(content);
       console.log('Content changed:', content);
     },
-  })
+  });
 
   // Use the tooltip plugin hook
-  useTooltipPlugin(editor)
+  useTooltipPlugin(editor);
 
   if (!editor) {
-    return null
+    return null;
   }
 
   return (
-      <Fragment>
-        <div className="editor-container">
-          <TitleButton editor={editor} />
+    <Fragment>
+      <div className='editor-container'>
+        <TitleButton editor={editor} />
 
-          <EditorContent editor={editor} />
-          {
-              state &&  (
-                  <div dangerouslySetInnerHTML={{ __html: state }} />
-              )
-          }
-        </div>
-      </Fragment>
+        <EditorContent editor={editor} />
+      </div>
+    </Fragment>
+  );
+};
 
-  )
-}
-
-export default TiptapEditor
+export default TiptapEditor;
